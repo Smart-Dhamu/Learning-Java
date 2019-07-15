fb65209d38e9004658282d39109cace24a56e32f
Smart-Dhamu <40119869+Smart-Dhamu@users.noreply.github.com>
2019-07-15 23:05:59 +0530

    First Commit

---
+++ fun.java
@@ -0,0 +1,11 @@
+class fun
+{
+	public static void main(String args[])
+	{
+			hello();
+	}
+public static void hello()
+{
+	System.out.println("Hello world!");
+}
+}
---
+++ ret.java
@@ -0,0 +1,12 @@
+class ret
+{
+	public static void main(String args[])
+	{
+		String str=hello();
+		System.out.println("The returned String is: "+str);
+	}
+	public static String hello()
+	{
+		return "Hello world!";
+	}
+}
